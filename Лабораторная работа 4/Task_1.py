1799475c0640c89e50d0ecab13081e2e53890431
Ilia <88272388+M-ilia@users.noreply.github.com>
2024-12-24 16:49:33 +0500

    Update Task_1.py

--- Лабораторная работа 4/Task_1.py
+++ Лабораторная работа 4/Task_1.py
@@ -6,8 +6,7 @@ file = 'input.json'
 def task(file) -> float:
     sum_ = 0
     with open(file) as f:
-        for line in json.load(f):
-            sum_ += line["score"] * line["weight"]
+        sum_ = sum([line["score"] * line["weight"] for line in json.load(f)])
     return sum_
 
 print(f'{task(file):.3f}')
